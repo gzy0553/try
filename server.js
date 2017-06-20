2a061879df5101d478c55d011b3a07309e1ee58a
gzy0553 <gzy0553@gmail.com>
2017-06-19 18:24:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var port=process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/',function(req,res){
-console.log('hello from server');
- res.render('./public/index.html');
-});
+
 
 app.get('/match',function(req,res){
 console.log('hello from server');
